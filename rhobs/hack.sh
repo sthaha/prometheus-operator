23ea086d2bc36d8862f3215d4452a0b72cdff389
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2022-09-06 13:01:56 +1000

    0.58.0-rhobs1

--- rhobs/hack.sh
+++ rhobs/hack.sh
@@ -8,6 +8,44 @@ declare PROJECT_ROOT
 PROJECT_ROOT="$(cd "$(dirname "$SCRIPT_PATH")/.." && pwd)"
 
 
+bumpup_version(){
+  # get all tags with
+  local version=$(head -n1 VERSION)
+  # remove any trailing rhobs
+  local upstream_version=${version//-rhobs*}
+  echo "found upstream version: $upstream_version"
+
+  # find git tags with
+  local patch
+  # git tag | grep "^v$upstream_version-rhobs" | wc -l
+  # NOTE: grep || true prevents grep from setting non-zero exit code
+  # if there are no -rhobs tag
+
+  patch=$( git tag | { grep "^v$upstream_version-rhobs" || true; } | wc -l )
+  (( patch+=1 ))
+
+  rhobs_version="$upstream_version-rhobs$patch"
+
+  echo "Updating version to $rhobs_version"
+  echo $rhobs_version > VERSION
+}
+
+publish_images() {
+  local version=$1; shift
+
+  local image_operator="quay.io/sthaha/rhobs-prometheus-operator"
+  local image_webhook="quay.io/sthaha/rhobs-po-admission-webhook"
+  local tag="v$version"
+
+  make image IMAGE_OPERATOR="$image_operator" \
+    IMAGE_WEBHOOK="$image_webhook" \
+    TAG="$tag"
+
+  docker push "$image_operator:$tag"
+  docker push "$image_webhook:$tag"
+}
+
+
 main() {
   # all files are relative to the root of the project
   cd "$PROJECT_ROOT"
@@ -25,8 +63,19 @@ main() {
       -e 's|+kubebuilder:resource:categories="prometheus-operator".*|+kubebuilder:resource:categories="rhobs-prometheus-operator"|g' \
   {} \;
 
+  bumpup_version
+
+  local version
+  version=$(head -n1 VERSION)
+
+
   make generate
+  publish_images "$version"
+
   return $?
+
 }
 
 main "$@"
+
+##
